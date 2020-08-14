9399782a1ce6c31cc40bd621b8b7a46239f054ea
Benjamin Kwame Ampah <58374644+benjamin2310@users.noreply.github.com>
2020-08-14 09:34:01 +0000

    Update project1.py

--- project1.py
+++ project1.py
@@ -32,7 +32,7 @@ while True:
     # Take input from the user
     choice = input("Enter choice(1/2/3/4): ")
 
-    # Check if choice is one of the four options
+    # Check if choice is one of the four options, the you proceed
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
